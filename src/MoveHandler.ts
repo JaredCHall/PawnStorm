8c61c19d59b05dcadced7dbd4d0ef28b8392d991
Jared Hall <hall.jared.c@gmail.com>
2024-03-30 16:57:15 -0500

    save work. got some parallel ideas going here

---
+++ src/MoveHandler.ts
@@ -0,0 +1,158 @@
+import {Board120, Color, PieceType, Square} from "./Board120.ts";
+import {Move, MoveFlag, MoveType} from "./Move.ts";
+
+export enum CastlingRight { // 4 bits
+    K= 0b0001,
+    Q= 0b0010,
+    k= 0b0100,
+    q= 0b1000
+}
+
+export class CastlingMoveInfo {
+    static readonly sideMask = new Uint8Array([0b1100, 0b0011])
+    static readonly kingSquare = new Uint8Array([Square.e1, Square.e8])
+
+    static readonly typeMap = new Uint8Array(16)
+
+
+    static {
+        this.typeMap[CastlingRight.K] = 0
+        this.typeMap[CastlingRight.Q] = 1
+        this.typeMap[CastlingRight.k] = 2
+        this.typeMap[CastlingRight.q] = 3
+    }
+
+    static readonly typeByKingNewSquare = new Uint8Array(120)
+    static readonly kingNewSquare = new Uint8Array([Square.g1, Square.c1, Square.g8, Square.g1])
+
+    static {
+        for(let i=0;i<4;i++){
+            this.typeByKingNewSquare[this.kingNewSquare[i]] = i
+        }
+    }
+
+    static readonly rookSquares = [
+        [Square.h1, Square.f1], [Square.a1, Square.d1],
+        [Square.h8, Square.f8], [Square.a8, Square.d8]
+    ]
+    static readonly emptySquares = [
+        [Square.f1, Square.g1], [Square.d1, Square.c1, Square.b1],
+        [Square.f8, Square.g8], [Square.d8, Square.c8, Square.b8],
+    ]
+    static readonly safeSquares = [
+        [Square.e1, Square.f1], [Square.e1, Square.d1],
+        [Square.e8, Square.f8], [Square.e8, Square.d8],
+    ]
+
+}
+
+
+export class BoardState {
+    sideToMove: Color = Color.White
+    castleRights: number = 0b0000
+    enPassantTarget: Square|0 = 0
+    halfMoveClock: number = 0
+
+    getCastlingRights(color: Color){
+        const rights = []
+        if(color & 1){ // black
+            if(this.castleRights & CastlingRight.k){rights.push(CastlingRight.k)}
+            if(this.castleRights & CastlingRight.q){rights.push(CastlingRight.q)}
+        }else{ //white
+            if(this.castleRights & CastlingRight.K){rights.push(CastlingRight.K)}
+            if(this.castleRights & CastlingRight.Q){rights.push(CastlingRight.Q)}
+        }
+        return rights
+    }
+    toggleSideToMove(): void {
+        this.sideToMove = this.sideToMove ? 0 : 1
+    }
+}
+
+export class MoveHandler extends Board120
+{
+
+    state: BoardState = new BoardState()
+    kingSquares = new Uint8Array(2)
+
+    enPassantTargets = new Uint8Array([
+         0,  0,  0,  0,  0,  0,  0,  0,
+         0,  0,  0,  0,  0,  0,  0,  0,
+         0,  0,  0,  0,  0,  0,  0,  0,
+        41, 42, 43, 44, 45, 46, 47, 48,
+        71, 72, 73, 74, 75, 76, 77, 78,
+         0,  0,  0,  0,  0,  0,  0,  0,
+         0,  0,  0,  0,  0,  0,  0,  0,
+         0,  0,  0,  0,  0,  0,  0,  0,
+    ])
+
+    makeMove(move: Move)
+    {
+        this.squareList[move.from] = 0
+        const movingType = move.moving >> 1
+        const movingColor = move.moving & 1
+        this.state.toggleSideToMove()
+
+        // handle pawn moves
+        if(movingType & PieceType.Pawn || movingType & PieceType.BPawn){
+            this.state.halfMoveClock = 0
+            if(move.flag == MoveType.DoublePawnPush){
+                this.squareList[move.to] = move.moving
+                this.state.enPassantTarget = this.enPassantTargets[this.square64Indexes[move.to]]
+            }else if(move.flag == MoveType.EnPassant){
+                this.squareList[move.to] = move.moving
+                this.squareList[this.state.enPassantTarget] = 0
+            }else if(move.flag & MoveFlag.Promotion){
+                const hasFlag1 = move.flag & MoveFlag.Flag2
+                const hasFlag2 = move.flag & MoveFlag.Flag2
+                if(hasFlag1){
+                    if(hasFlag2){
+                        this.squareList[move.to] = PieceType.Queen << 1 | movingColor
+                    }else{
+                        this.squareList[move.to] = PieceType.Bishop << 1 | movingColor
+                    }
+                }else if(hasFlag2) {
+                    this.squareList[move.to] = PieceType.Rook << 1 | movingColor
+                }else{
+                    this.squareList[move.to] = PieceType.Knight << 1 | movingColor
+                }
+            }
+            return
+        }
+
+        // moves by other pieces
+        this.squareList[move.to] = move.moving
+        this.state.enPassantTarget = 0
+
+        if(move.flag & MoveFlag.Capture){
+            this.state.halfMoveClock = 0
+        }else{
+            this.state.halfMoveClock++
+        }
+
+        // rook moves
+        if(movingType & PieceType.Rook){
+            this.state.getCastlingRights(movingColor).forEach((right) => {
+                if(CastlingMoveInfo.rookSquares[CastlingMoveInfo.typeMap[right]][0] == move.from){
+                    this.state.castleRights &= ~right
+                }
+            })
+            return
+        }
+
+        // king moves
+        if(movingType & PieceType.King){
+            // either castle type
+            if(move.flag & MoveFlag.Flag1){
+                const type = CastlingMoveInfo.typeByKingNewSquare[move.to]
+                const rookSquares = CastlingMoveInfo.rookSquares[type]
+                this.squareList[rookSquares[0]] = 0
+                this.squareList[rookSquares[1]] = PieceType.Rook << 1 | movingColor
+            }
+            this.state.castleRights &= ~CastlingMoveInfo.sideMask[movingColor]
+        }
+
+    }
+
+
+}
---
+++ tests/Board120Test.ts
@@ -0,0 +1,135 @@
+import {assertEquals} from "https://deno.land/std@0.219.0/assert/assert_equals.ts";
+import {Board120, Square, Piece} from "../src/Board120.ts";
+import {binToString} from "../src/Utils.ts";
+
+const board = new Board120()
+
+Deno.test('it initializes square indexes correctly' , () => {
+    for(let i=0; i<64; i++){
+        assertEquals(board.square64Indexes[board.square120Indexes[i]], i)
+    }
+})
+
+
+Deno.test('it initializes square list correctly', () => {
+
+    const oob = Square.Invalid
+    const expectedSquareList = [
+        oob, oob, oob, oob, oob, oob, oob, oob, oob, oob,
+        oob, oob, oob, oob, oob, oob, oob, oob, oob, oob,
+        oob,   0,   0,   0,   0,   0,   0,   0,   0, oob,
+        oob,   0,   0,   0,   0,   0,   0,   0,   0, oob,
+        oob,   0,   0,   0,   0,   0,   0,   0,   0, oob,
+        oob,   0,   0,   0,   0,   0,   0,   0,   0, oob,
+        oob,   0,   0,   0,   0,   0,   0,   0,   0, oob,
+        oob,   0,   0,   0,   0,   0,   0,   0,   0, oob,
+        oob,   0,   0,   0,   0,   0,   0,   0,   0, oob,
+        oob,   0,   0,   0,   0,   0,   0,   0,   0, oob,
+        oob, oob, oob, oob, oob, oob, oob, oob, oob, oob,
+        oob, oob, oob, oob, oob, oob, oob, oob, oob, oob,
+    ]
+    assertEquals(Array.from(board.squareList), expectedSquareList, 'squareList is set correctly')
+})
+
+Deno.test('it initializes square ranks correctly', () => {
+    const expectedList = [
+        7, 7, 7, 7, 7, 7, 7, 7,
+        6, 6, 6, 6, 6, 6, 6, 6,
+        5, 5, 5, 5, 5, 5, 5, 5,
+        4, 4, 4, 4, 4, 4, 4, 4,
+        3, 3, 3, 3, 3, 3, 3, 3,
+        2, 2, 2, 2, 2, 2, 2, 2,
+        1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    ]
+    assertEquals(Array.from(board.squareRanks), expectedList, 'squaresRanks is set correctly')
+})
+
+Deno.test('it initializes square files correctly', () => {
+    const expectedList = [
+        0, 1, 2, 3, 4, 5, 6, 7,
+        0, 1, 2, 3, 4, 5, 6, 7,
+        0, 1, 2, 3, 4, 5, 6, 7,
+        0, 1, 2, 3, 4, 5, 6, 7,
+        0, 1, 2, 3, 4, 5, 6, 7,
+        0, 1, 2, 3, 4, 5, 6, 7,
+        0, 1, 2, 3, 4, 5, 6, 7,
+        0, 1, 2, 3, 4, 5, 6, 7,
+    ]
+    assertEquals(Array.from(board.squareFiles), expectedList, 'squareFiles is set correctly')
+})
+
+Deno.test('it initializes square distances correctly', () => {
+
+    assertEquals(board.squareDistances.length, 64, 'Distances generated for every square')
+    for(let i = 0;i<64;i++){
+        assertEquals(board.squareDistances[i].length, 64, `There are 64 expected distances for square with index: ${i}`)
+    }
+
+    // there's 4096 of these, so we will spot check some distances
+    assertEquals(board.getDistanceBetweenSquares(Square.e4, Square.h1), 3, 'calculates 3 king moves from e4 to h1')
+    assertEquals(board.getDistanceBetweenSquares(Square.e4, Square.a1), 4, 'calculates 3 king moves from e4 to a1')
+    assertEquals(board.getDistanceBetweenSquares(Square.a8, Square.h1), 7, 'calculates 7 king moves from a8 to h1')
+    assertEquals(board.getDistanceBetweenSquares(Square.d5, Square.e5), 1, 'calculates 1 king moves from d5 to e5')
+    assertEquals(board.getDistanceBetweenSquares(Square.d5, Square.d5), 0, 'calculates 0 king moves from d5 to d5')
+    assertEquals(board.getDistanceBetweenSquares(Square.a2, Square.a6), 4, 'calculates 4 king moves from a2 to a6')
+})
+
+const assertSquareEquals = (square: Square, pieceCode: number) => {
+    assertEquals(board.squareList[square], pieceCode, `Expected ${binToString(pieceCode,8)} on square ${square}`)
+}
+
+Deno.test('it sets board representation', () => {
+
+    board.setPieces('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
+
+    assertSquareEquals(Square.a8, Piece.BlackRook)
+    assertSquareEquals(Square.b8, Piece.BlackKnight)
+    assertSquareEquals(Square.c8, Piece.BlackBishop)
+    assertSquareEquals(Square.d8, Piece.BlackQueen)
+    assertSquareEquals(Square.e8, Piece.BlackKing)
+    assertSquareEquals(Square.f8, Piece.BlackBishop)
+    assertSquareEquals(Square.g8, Piece.BlackKnight)
+    assertSquareEquals(Square.h8, Piece.BlackRook)
+
+    assertSquareEquals(Square.a7, Piece.BlackPawn)
+    assertSquareEquals(Square.b7, Piece.BlackPawn)
+    assertSquareEquals(Square.c7, Piece.BlackPawn)
+    assertSquareEquals(Square.d7, Piece.BlackPawn)
+    assertSquareEquals(Square.e7, Piece.BlackPawn)
+    assertSquareEquals(Square.f7, Piece.BlackPawn)
+    assertSquareEquals(Square.g7, Piece.BlackPawn)
+    assertSquareEquals(Square.h7, Piece.BlackPawn)
+
+    assertSquareEquals(Square.a1, Piece.WhiteRook)
+    assertSquareEquals(Square.b1, Piece.WhiteKnight)
+    assertSquareEquals(Square.c1, Piece.WhiteBishop)
+    assertSquareEquals(Square.d1, Piece.WhiteQueen)
+    assertSquareEquals(Square.e1, Piece.WhiteKing)
+    assertSquareEquals(Square.f1, Piece.WhiteBishop)
+    assertSquareEquals(Square.g1, Piece.WhiteKnight)
+    assertSquareEquals(Square.h1, Piece.WhiteRook)
+
+    assertSquareEquals(Square.a2, Piece.WhitePawn)
+    assertSquareEquals(Square.b2, Piece.WhitePawn)
+    assertSquareEquals(Square.c2, Piece.WhitePawn)
+    assertSquareEquals(Square.d2, Piece.WhitePawn)
+    assertSquareEquals(Square.e2, Piece.WhitePawn)
+    assertSquareEquals(Square.f2, Piece.WhitePawn)
+    assertSquareEquals(Square.g2, Piece.WhitePawn)
+    assertSquareEquals(Square.h2, Piece.WhitePawn)
+
+    const emptySquares: number[] = [
+        Square.a6, Square.b6, Square.c6, Square.d6, Square.e6, Square.f6, Square.g6, Square.h6,
+        Square.a5, Square.b5, Square.c5, Square.d5, Square.e5, Square.f5, Square.g5, Square.h5,
+        Square.a4, Square.b4, Square.c4, Square.d4, Square.e4, Square.f4, Square.g4, Square.h4,
+        Square.a3, Square.b3, Square.c3, Square.d3, Square.e3, Square.f3, Square.g3, Square.h3,
+    ]
+    emptySquares.forEach((square) => {
+        assertSquareEquals(square, 0)
+    })
+})
+
+Deno.test('it renders board with highlights', () => {
+    board.render([Square.e1, Square.e8])
+})
