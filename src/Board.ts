e5864701c098ac8cd0730206473da77a614a559a
Jared Hall <hall.jared.c@gmail.com>
2024-03-30 01:29:16 -0500

    new moves implemented

--- src/Board.ts
+++ src/Board.ts
@@ -1,7 +1,7 @@
 
 
 //
-import {FenPieceMap, Piece, pieceRenderMap, Square, SquareIndexes, Color, PieceType} from "./Enums.ts";
+import {FenPieceMap, Piece, pieceRenderMap, Square, SquareIndexes, Color, PieceType, CastlingRight} from "./Enums.ts";
 import {
     bgBrightBlue,
     bgBrightGreen,
@@ -11,25 +11,64 @@ import {
     white
 } from "https://deno.land/std@0.219.1/fmt/colors.ts";
 import {Move, MoveType} from "./Move.ts";
+import {dumpBin} from "./Utils.ts";
 
+enum RayDirection {
+    N = -10, NE = -9, E = 1, SE = 11, S = 10, SW = 9, W = -1, NW = -11
+}
 
 class RayDirections {
-    static readonly cardinal: number[] = [-10, -1, 1, 10]
-    static readonly diagonal: number[] = [9, 11, -9, -11]
-    static readonly all: number[] = RayDirections.cardinal.concat(RayDirections.diagonal)
+    static readonly cardinal: number[] = [RayDirection.N, RayDirection.E, RayDirection.S, RayDirection.W]
+    static readonly ordinal: number[] = [RayDirection.NE, RayDirection.SE, RayDirection.SW, RayDirection.NW]
+    static readonly all: number[] = RayDirections.cardinal.concat(RayDirections.ordinal)
     static readonly knightMoves = [-21, -19,-12, -8, 8, 12, 19, 21]
 
-    // i[0]: max ray length
-    // i[1]: capture ray directions
-    // i[2]: quiet move ray directions (pawns only)
     static readonly pieceMap = {
+        // i[0]: max ray length
+        // i[1]: capture ray directions
         [PieceType.Knight]: [1, RayDirections.knightMoves],
         [PieceType.Rook]:   [7, RayDirections.cardinal],
-        [PieceType.Bishop]: [7, RayDirections.diagonal],
+        [PieceType.Bishop]: [7, RayDirections.ordinal],
         [PieceType.Queen]:  [7, RayDirections.all],
         [PieceType.King]:   [1, RayDirections.all],
-        [PieceType.Pawn]:   [1, [-11 , -9], [10, 20]],
-        [PieceType.BPawn]:  [1, [11, 9], [10, 20]],
+        
+        // i[0]: double-move rank
+        // i[1]: promotion rank
+        // i[2]: capture move ray directions
+        // i[3]: quiet move ray directions
+        [PieceType.Pawn]:   [2, 7, [RayDirection.NW , RayDirection.NE], [RayDirection.N, RayDirection.N * 2]],
+        [PieceType.BPawn]:  [7, 2, [RayDirection.SW , RayDirection.SE], [RayDirection.S, RayDirection.S * 2]],
+    }
+
+    static onSameRay(square1: Square, square2: Square): boolean
+    {
+        throw new Error('not implemented')
+    }
+}
+
+export class CastlingData {
+    static readonly sideMask: Record<Color, number> = {
+        [Color.White]: 0b1100,
+        [Color.Black]: 0b0011,
+    }
+    static readonly kingSquare: Record<Color, number> = {
+        [Color.White]: Square.e1,
+        [Color.Black]: Square.e8,
+    }
+    // i[0] - king
+    // i[1] - rook
+    // i[2] - squares that must be empty
+    // i[3] - squares that must be safe
+    static readonly fromToSquares = {
+        [CastlingRight.K]: [[Square.e1, Square.g1], [Square.h1, Square.f1], [Square.f1, Square.g1], [Square.e1, Square.f1, Square.g1]],
+        [CastlingRight.Q]: [[Square.e1, Square.c1], [Square.a1, Square.d1], [Square.d1, Square.c1, Square.b1], [Square.e1, Square.d1, Square.c1]],
+        [CastlingRight.k]: [[Square.e8, Square.g8], [Square.h8, Square.f8], [Square.f8, Square.g8], [Square.e8, Square.f8, Square.g8]],
+        [CastlingRight.q]: [[Square.e8, Square.c8], [Square.a8, Square.d8], [Square.d8, Square.c8, Square.b8], [Square.e8, Square.d8, Square.c8]],
+    }
+
+    static readonly MoveTypes = {
+        [CastlingRight.K]: MoveType.CastleShort, [CastlingRight.Q]: MoveType.CastleLong,
+        [CastlingRight.k]: MoveType.CastleShort, [CastlingRight.q]: MoveType.CastleLong,
     }
 }
 
@@ -38,6 +77,30 @@ export class BoardState {
     castleRights: number = 0b0000
     enPassantTarget: Square|0 = 0
     halfMoveClock: number = 0
+
+    getCastlingRights(color: Color){
+        const rights = []
+        if(color & 1){
+            // black
+            if(this.castleRights & CastlingRight.k){
+                rights.push(CastlingRight.k)
+            }
+            if(this.castleRights & CastlingRight.q){
+                rights.push(CastlingRight.q)
+            }
+        }else{
+            //white
+            if(this.castleRights & CastlingRight.K){
+                rights.push(CastlingRight.K)
+            }
+            if(this.castleRights & CastlingRight.Q){
+                rights.push(CastlingRight.Q)
+            }
+
+        }
+        return rights
+    }
+
 }
 
 export class Board {
@@ -51,8 +114,11 @@ export class Board {
      *  bits 7 - 2 are the piece type
      *  bit 1 is the piece color
      */
-    squareList = new Uint8Array(120) // first bit is if square is out-of-bounds. next
-    squareIndexMap: Uint8Array =  new Uint8Array(64)
+    readonly squareList = new Uint8Array(120) // first bit is if square is out-of-bounds. next
+    readonly squareIndexMap: Uint8Array =  new Uint8Array(64)
+    readonly squaresRanks = new Uint8Array(120)
+
+    state = new BoardState()
 
     constructor() {
         // initialize all squares to invalid
@@ -60,9 +126,15 @@ export class Board {
             this.squareList[i] = Square.Invalid
         }
         // set valid squares to empty and build map of indexes
+        let row = 8
         for(let i = 0; i < 64; i++){
-            this.squareIndexMap[i] = SquareIndexes[i]
-            this.squareList[SquareIndexes[i]] = 0
+            const squareIndex = SquareIndexes[i]
+            this.squareIndexMap[i] = squareIndex
+            this.squareList[squareIndex] = 0
+            this.squaresRanks[squareIndex] = row
+            if((i + 1) % 8 == 0){
+                row--
+            }
         }
     }
 
@@ -91,27 +163,27 @@ export class Board {
         }
     }
 
+
     getMovesForSquare(from: Square, moving: Piece): Move[]
     {
         const moves: Move[] = []
         const type = moving >> 1
-        const color = moving & 1
+        const color: Color = moving & 1
 
-        if(type & PieceType.Pawn || type & Piece.BlackPawn){
-           throw new Error('not implemented')
+        if(type & PieceType.Pawn || type & PieceType.BPawn){
+            return this.#getPawnMoves(from, moving, type, color)
         }
 
         // @ts-ignore
         const rayDirections = RayDirections.pieceMap[type]
         const offsets = rayDirections[1]
         const maxRayLength = rayDirections[0]
-
         for(let i = 0; i<offsets.length;i++) {
             const offset = offsets[i]
             for (let j = 1; j <= maxRayLength; j++) {
                 const to: number = from + j * offset
                 const captured = this.squareList[to]
-                if (captured & Square.Invalid) {
+                if (captured == Square.Invalid) {
                     break // square out of bounds
                 }
 
@@ -127,8 +199,104 @@ export class Board {
                 }
 
                 moves.push(new Move(from, to, moving, captured, MoveType.Capture))
+                break
             }
         }
+        if(!(type & PieceType.King) || from != CastlingData.kingSquare[color]){
+            return moves
+        }
+
+
+        //handle castling moves
+        this.state.getCastlingRights(color).forEach((right) => {
+            const [kingSquares, rookSquares, emptySquares, safeSquares] = CastlingData.fromToSquares[right]
+
+
+            if(this.squareList[rookSquares[0]] != (PieceType.Rook << 1 | color)){
+                return
+            }
+            if(!emptySquares.every((square)=> this.squareList[square] == 0)){
+                return
+            }
+            // TODO: Check squares are safe here?
+            moves.push(new Move(from, kingSquares[1], moving, 0, CastlingData.MoveTypes[right]))
+        })
+
+        return moves
+    }
+
+    #addCastlingMoves(from: Square, moving: Piece, color: Color)
+    {
+        const rights = this.state.castleRights & CastlingData.sideMask[color]
+        if(rights == 0){
+            return
+        }
+    }
+
+
+    #getPawnMoves(from: Square, moving: Piece, type: PieceType, color: Color): Move[] {
+        const moves: Move[] = []
+        const rayDirections = RayDirections.pieceMap[type]
+        const [
+            doubleMoveRank,
+            promotesFromRank,
+            captureOffsets,
+            quietOffsets
+        ] = rayDirections
+        const rank = this.squaresRanks[from]
+
+        // Quiet moves
+        // @ts-ignore it's fine
+        let to: number = from + quietOffsets[0]
+        if(this.squareList[to] == 0){
+            if(promotesFromRank == rank){
+                moves.push(new Move(from, to, moving, 0, MoveType.KnightPromote))
+                moves.push(new Move(from, to, moving, 0, MoveType.BishopPromote))
+                moves.push(new Move(from, to, moving, 0, MoveType.RookPromote))
+                moves.push(new Move(from, to, moving, 0, MoveType.QueenPromote))
+            }else{
+                moves.push(new Move(from, to, moving, 0, MoveType.Quiet))
+                if(rank == doubleMoveRank){
+                    // @ts-ignore also fine
+                    to = from + quietOffsets[1]
+                    if(this.squareList[to] == 0){
+                        moves.push(new Move(from, to, moving, 0, MoveType.DoublePawnPush))
+                    }
+                }
+            }
+        }
+
+        // Capture moves
+        const promotes = promotesFromRank == rank
+        for(let i=0;i<2;i++){
+            // @ts-ignore it's fine
+            const to: number = from + captureOffsets[i]
+            const captured = this.squareList[to]
+
+            if(captured == 0){
+                if(to == this.state.enPassantTarget){
+                    // @ts-ignore it's fine - captured piece is one quiet move behind the pawn
+                    moves.push(new Move(from, to, moving, this.squareList[to + -1 * quietOffsets[0]], MoveType.EnPassant))
+                }
+                // cannot capture empty square if it's not en-passant.
+                continue
+            }
+            if(captured == Square.Invalid // cannot capture out of bounds square
+                || (captured & 1) == color // cannot capture friendly piece
+            ){
+                continue
+            }
+
+            if(promotes){
+                moves.push(new Move(from, to, moving, captured, MoveType.KnightPromote | MoveType.Capture))
+                moves.push(new Move(from, to, moving, captured, MoveType.BishopPromote | MoveType.Capture))
+                moves.push(new Move(from, to, moving, captured, MoveType.RookPromote | MoveType.Capture))
+                moves.push(new Move(from, to, moving, captured, MoveType.QueenPromote | MoveType.Capture))
+            }else{
+                moves.push(new Move(from, to, moving, captured, MoveType.Capture))
+            }
+        }
+
         return moves
     }
 
--- tests/BoardTest.ts
+++ tests/BoardTest.ts
@@ -1,8 +1,9 @@
 import {Board} from "../src/Board.ts";
-import {Piece, Square} from "../src/Enums.ts";
+import {CastlingRight, Piece, Square} from "../src/Enums.ts";
 import {assertEquals} from "https://deno.land/std@0.219.0/assert/assert_equals.ts";
 import {binToString} from "../src/Utils.ts";
 import {assertArrayIncludes} from "https://deno.land/std@0.219.0/assert/assert_array_includes.ts";
+import {Move, MoveType} from "../src/Move.ts";
 const board = new Board()
 
 
@@ -10,13 +11,27 @@ const assertSquareEquals = (square: Square, pieceCode: number) => {
     assertEquals(board.squareList[square], pieceCode, `Expected ${binToString(pieceCode,8)} on square ${square}`)
 }
 
-const assertSquareListsMatch = (actual: Square[], expected: Square[]) => {
-    assertArrayIncludes(actual, expected)
-    const extras = actual.filter((square) => !expected.includes(square))
-    assertEquals(extras.length, 0, `Unexpected squares in list: ${extras.join(', ')}`)
+const assertMatchesSquareList = (actual: Move[], expected: Square[], message='Move list matches expected square names') => {
+    const actualSquares = actual.map((move) => move.to)
+    assertArrayIncludes(actualSquares, expected, message)
+    const extras = actualSquares.filter((square) => !expected.includes(square))
+    assertEquals(extras.length, 0, `${message}: Unexpected squares in list: ${extras.join(', ')}`)
 }
 
-Deno.test('it initializes square list correct', () => {
+const assertMatchesMoveList = (actual: Move[], expected: Move[], message: string = 'Move list matches expected Move list') => {
+    assertEquals(actual, expected, message)
+}
+
+const genMoves = (piecePositions: string, from: Square, enPassantTarget: Square|0=0, castleRights: number = 0): Move[] => {
+    board.setPieces(piecePositions)
+    board.state.enPassantTarget = enPassantTarget
+    board.state.castleRights = castleRights
+    const moves = board.getMovesForSquare(from, board.squareList[from])
+    board.render(moves.map((move) => move.to))
+    return moves
+}
+
+Deno.test('it initializes square list correctly', () => {
     
     const oob = Square.Invalid
     const expectedSquareList = [
@@ -36,6 +51,25 @@ Deno.test('it initializes square list correct', () => {
     assertEquals(Array.from(board.squareList), expectedSquareList, 'squareList is set correctly')
 })
 
+Deno.test('it initializes square ranks correctly', () => {
+
+    const oob = 0
+    const expectedList = [
+        oob, oob, oob, oob, oob, oob, oob, oob, oob, oob,
+        oob, oob, oob, oob, oob, oob, oob, oob, oob, oob,
+        oob,   8,   8,   8,   8,   8,   8,   8,   8, oob,
+        oob,   7,   7,   7,   7,   7,   7,   7,   7, oob,
+        oob,   6,   6,   6,   6,   6,   6,   6,   6, oob,
+        oob,   5,   5,   5,   5,   5,   5,   5,   5, oob,
+        oob,   4,   4,   4,   4,   4,   4,   4,   4, oob,
+        oob,   3,   3,   3,   3,   3,   3,   3,   3, oob,
+        oob,   2,   2,   2,   2,   2,   2,   2,   2, oob,
+        oob,   1,   1,   1,   1,   1,   1,   1,   1, oob,
+        oob, oob, oob, oob, oob, oob, oob, oob, oob, oob,
+        oob, oob, oob, oob, oob, oob, oob, oob, oob, oob,
+    ]
+    assertEquals(Array.from(board.squaresRanks), expectedList, 'squaresRanks is set correctly')
+})
 
 Deno.test('it sets board representation', () => {
     
@@ -89,12 +123,9 @@ Deno.test('it sets board representation', () => {
 })
 
 
-Deno.test('it generates quiet moves for queen', () => {
-    board.setPieces('8/8/8/4Q3/8/8/8/8')
-
-    const moves = board.getMovesForSquare(Square.e5, Piece.WhiteQueen).map((move) => move.to)
-    board.render(moves)
-    assertSquareListsMatch(moves, [
+Deno.test('it generates moves for queen', () => {
+    let moves = genMoves('8/8/8/4Q3/8/8/8/8', Square.e5)
+    assertMatchesSquareList(moves, [
         Square.e6, Square.e7, Square.e8, // N
         Square.f5, Square.g5, Square.h5, // E
         Square.e4, Square.e3, Square.e2, Square.e1, // S
@@ -103,54 +134,171 @@ Deno.test('it generates quiet moves for queen', () => {
         Square.f4, Square.g3, Square.h2, // SE
         Square.d4, Square.c3, Square.b2, Square.a1, // SW
         Square.d6, Square.c7, Square.b8 // NW
-    ])
+    ], 'quiet moves on open board')
+
+    moves = genMoves('6RQ/7r/5P2/8/8/8/8/8', Square.h8)
+    assertMatchesSquareList(moves, [
+        Square.g7, Square.h7
+    ], 'moves on edge of the board')
 });
 
-Deno.test('it generates quiet moves for rook', () => {
-    board.setPieces('8/8/8/4R3/8/8/8/8')
-
-    const moves = board.getMovesForSquare(Square.e5, Piece.WhiteRook).map((move) => move.to)
-    board.render(moves)
-    assertSquareListsMatch(moves, [
+Deno.test('it generates moves for rook', () => {
+    let moves = genMoves('8/8/8/4R3/8/8/8/8', Square.e5)
+    assertMatchesSquareList(moves, [
         Square.e6, Square.e7, Square.e8, // N
         Square.f5, Square.g5, Square.h5, // E
         Square.e4, Square.e3, Square.e2, Square.e1, // S
         Square.d5, Square.c5, Square.b5, Square.a5, // W
-    ])
+    ], 'quiet moves on open board')
+
+    moves = genMoves('5R1R/7r/5P2/8/8/8/8/8', Square.h8)
+    assertMatchesSquareList(moves, [
+        Square.g8, Square.h7
+    ], 'moves on edge of the board')
 });
 
-Deno.test('it generates quiet moves for bishop', () => {
-    board.setPieces('8/8/8/4B3/8/8/8/8')
-
-    const moves = board.getMovesForSquare(Square.e5, Piece.WhiteBishop).map((move) => move.to)
-    board.render(moves)
-    assertSquareListsMatch(moves,[
+Deno.test('it generates moves for bishop', () => {
+    let moves = genMoves('8/8/8/4B3/8/8/8/8', Square.e5)
+    assertMatchesSquareList(moves,[
         Square.f6, Square.g7, Square.h8, // NE
         Square.f4, Square.g3, Square.h2, // SE
         Square.d4, Square.c3, Square.b2, Square.a1, // SW
         Square.d6, Square.c7, Square.b8 // NW
-    ])
+    ], 'quiet moves on open board')
+
+    moves = genMoves('5P2/8/7B/6r1/8/8/8/8', Square.h6)
+    assertMatchesSquareList(moves, [
+        Square.g7, Square.g5
+    ], 'moves on edge of the board')
 });
 
-
-Deno.test('it generates quiet moves for knight', () => {
-    board.setPieces('8/8/8/4N3/8/8/8/8')
-
-    const moves = board.getMovesForSquare(Square.e5, Piece.WhiteKnight).map((move) => move.to)
-    board.render(moves)
-    assertSquareListsMatch(moves,[
+Deno.test('it generates moves for knight', () => {
+    let moves = genMoves('8/8/8/4N3/8/8/8/8', Square.e5)
+    assertMatchesSquareList(moves,[
         Square.c6, Square.d7, Square.f7, Square.g6,
         Square.c4, Square.d3, Square.f3, Square.g4
-    ])
+    ], 'quiet moves on open board')
+
+    moves = genMoves('7N/5P2/6r1/8/8/8/8/8', Square.h8)
+    assertMatchesSquareList(moves, [
+        Square.g6
+    ], 'moves on edge of the board')
 });
 
-Deno.test('it generates quiet moves for king', () => {
-    board.setPieces('8/8/8/4K3/8/8/8/8')
-
-    const moves = board.getMovesForSquare(Square.e5, Piece.WhiteKing).map((move) => move.to)
-    board.render(moves)
-    assertSquareListsMatch(moves,[
+Deno.test('it generates moves for king', () => {
+    let moves = genMoves('8/8/8/4K3/8/8/8/8', Square.e5)
+    assertMatchesSquareList(moves,[
         Square.e6, Square.f5, Square.e4, Square.d5,
         Square.f6, Square.f4, Square.d4, Square.d6,
-    ])
+    ], 'quiet moves on open board')
 });
+
+Deno.test('it generates castles as white', () => {
+    let moves = genMoves('8/8/8/8/8/8/3PPP2/R3K2R', Square.e1, 0, CastlingRight.K | CastlingRight.Q)
+    assertMatchesMoveList(moves,[
+        new Move(Square.e1, Square.f1, Piece.WhiteKing, 0, 0),
+        new Move(Square.e1, Square.d1, Piece.WhiteKing, 0, 0),
+        new Move(Square.e1, Square.g1, Piece.WhiteKing, 0, MoveType.CastleShort),
+        new Move(Square.e1, Square.c1, Piece.WhiteKing, 0, MoveType.CastleLong),
+    ], 'Can castle either side')
+})
+
+Deno.test('it generates quiet moves for white pawn', () => {
+    let moves = genMoves('8/8/8/8/8/8/4P3/8', Square.e2)
+    assertMatchesMoveList(moves,[
+        new Move(Square.e2, Square.e3, Piece.WhitePawn, 0, 0),
+        new Move(Square.e2, Square.e4, Piece.WhitePawn, 0, MoveType.DoublePawnPush)
+    ], 'Pawn has expected moves from starting rank')
+
+    moves = genMoves('8/4P3/8/8/8/8/8/8', Square.e7)
+    assertMatchesMoveList(moves,[
+        new Move(Square.e7, Square.e8, Piece.WhitePawn, 0, MoveType.KnightPromote),
+        new Move(Square.e7, Square.e8, Piece.WhitePawn, 0, MoveType.BishopPromote),
+        new Move(Square.e7, Square.e8, Piece.WhitePawn, 0, MoveType.RookPromote),
+        new Move(Square.e7, Square.e8, Piece.WhitePawn, 0, MoveType.QueenPromote),
+    ], 'Pawn has expected promotions')
+});
+
+Deno.test('it generates quiet moves for black pawn', () => {
+    let moves = genMoves('8/4p3/8/8/8/8/8/8', Square.e7)
+    assertMatchesMoveList(moves,[
+        new Move(Square.e7, Square.e6, Piece.BlackPawn, 0, 0),
+        new Move(Square.e7, Square.e5, Piece.BlackPawn, 0, MoveType.DoublePawnPush)
+    ], 'Pawn has expected moves from starting rank')
+
+    moves = genMoves('8/8/8/8/8/8/4p3/8', Square.e2)
+    assertMatchesMoveList(moves,[
+        new Move(Square.e2, Square.e1, Piece.BlackPawn, 0, MoveType.KnightPromote),
+        new Move(Square.e2, Square.e1, Piece.BlackPawn, 0, MoveType.BishopPromote),
+        new Move(Square.e2, Square.e1, Piece.BlackPawn, 0, MoveType.RookPromote),
+        new Move(Square.e2, Square.e1, Piece.BlackPawn, 0, MoveType.QueenPromote),
+    ], 'Pawn has expected promotions')
+});
+
+Deno.test('it generates captures and en-passant for white pawn', () => {
+    let moves = genMoves('8/8/8/4pn2/4P3/8/8/8', Square.e4)
+    assertMatchesMoveList(moves,[
+        new Move(Square.e4, Square.f5, Piece.WhitePawn, Piece.BlackKnight, MoveType.Capture)
+    ], 'Pawn has expected capture in middle of the board')
+
+    moves = genMoves('8/8/8/6pn/7P/8/8/8', Square.h4)
+    assertMatchesMoveList(moves,[
+        new Move(Square.h4, Square.g5, Piece.WhitePawn, Piece.BlackPawn, MoveType.Capture)
+    ], 'Pawn has expected capture on edge of the board')
+
+    moves = genMoves('5QRn/6P1/8/8/8/8/8/8', Square.g7)
+    assertMatchesMoveList(moves,[
+        new Move(Square.g7, Square.h8, Piece.WhitePawn, Piece.BlackKnight, MoveType.KnightPromote | MoveType.Capture),
+        new Move(Square.g7, Square.h8, Piece.WhitePawn, Piece.BlackKnight, MoveType.BishopPromote | MoveType.Capture),
+        new Move(Square.g7, Square.h8, Piece.WhitePawn, Piece.BlackKnight, MoveType.RookPromote | MoveType.Capture),
+        new Move(Square.g7, Square.h8, Piece.WhitePawn, Piece.BlackKnight, MoveType.QueenPromote | MoveType.Capture),
+    ], 'Pawn has expected capture promotions')
+
+    moves = genMoves('8/8/6b1/4pP2/8/8/8/8', Square.f5, Square.e6)
+    assertMatchesMoveList(moves,[
+        new Move(Square.f5, Square.f6, Piece.WhitePawn, 0, 0),
+        new Move(Square.f5, Square.e6, Piece.WhitePawn, Piece.BlackPawn, MoveType.EnPassant),
+        new Move(Square.f5, Square.g6, Piece.WhitePawn, Piece.BlackBishop, MoveType.Capture),
+
+    ], 'Pawn has expected en-passant capture in middle of the board')
+
+    moves = genMoves('8/8/N7/Pp6/8/8/8/8', Square.a5, Square.b6)
+    assertMatchesMoveList(moves,[
+        new Move(Square.a5, Square.b6, Piece.WhitePawn, Piece.BlackPawn, MoveType.EnPassant),
+
+    ], 'Pawn has expected en-passant capture on edge of the board')
+})
+
+Deno.test('it generates captures and en-passant for black pawn', () => {
+    let moves = genMoves('8/8/8/8/4p3/4PN2/8/8', Square.e4)
+    assertMatchesMoveList(moves,[
+        new Move(Square.e4, Square.f3, Piece.BlackPawn, Piece.WhiteKnight, MoveType.Capture)
+    ], 'Pawn has expected capture in middle of the board')
+
+    moves = genMoves('8/8/8/8/7p/6PN/8/8', Square.h4)
+    assertMatchesMoveList(moves,[
+        new Move(Square.h4, Square.g3, Piece.BlackPawn, Piece.WhitePawn, MoveType.Capture)
+    ], 'Pawn has expected capture on edge of the board')
+
+    moves = genMoves('8/8/8/8/8/8/6p1/5qrN', Square.g2)
+    assertMatchesMoveList(moves,[
+        new Move(Square.g2, Square.h1, Piece.BlackPawn, Piece.WhiteKnight, MoveType.KnightPromote | MoveType.Capture),
+        new Move(Square.g2, Square.h1, Piece.BlackPawn, Piece.WhiteKnight, MoveType.BishopPromote | MoveType.Capture),
+        new Move(Square.g2, Square.h1, Piece.BlackPawn, Piece.WhiteKnight, MoveType.RookPromote | MoveType.Capture),
+        new Move(Square.g2, Square.h1, Piece.BlackPawn, Piece.WhiteKnight, MoveType.QueenPromote | MoveType.Capture),
+    ], 'Pawn has expected capture promotions')
+
+    moves = genMoves('8/8/8/8/4Pp2/6B1/8/8', Square.f4, Square.e3)
+    assertMatchesMoveList(moves,[
+        new Move(Square.f4, Square.f3, Piece.BlackPawn, 0, 0),
+        new Move(Square.f4, Square.e3, Piece.BlackPawn, Piece.WhitePawn, MoveType.EnPassant),
+        new Move(Square.f4, Square.g3, Piece.BlackPawn, Piece.WhiteBishop, MoveType.Capture),
+
+    ], 'Pawn has expected en-passant capture in middle of the board')
+
+    moves = genMoves('8/8/8/8/pP6/n7/8/8', Square.a4, Square.b3)
+    assertMatchesMoveList(moves,[
+        new Move(Square.a4, Square.b3, Piece.BlackPawn, Piece.WhitePawn, MoveType.EnPassant),
+
+    ], 'Pawn has expected en-passant capture on edge of the board')
+})
