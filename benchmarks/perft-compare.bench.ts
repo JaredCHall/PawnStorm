e984f5acae3faf42325672daf56eaac592ed1711
Jared Hall <hall.jared.c@gmail.com>
2024-10-06 13:50:30 -0500

    Update benchmarks

--- benchmarks/perft-compare.bench.ts
+++ benchmarks/perft-compare.bench.ts
@@ -3,13 +3,14 @@ import {StockfishInterface} from "../src/Engine/StockfishInterface.ts";
 import {buildStandardBoard as build_deno_chess_board} from "https://deno.land/x/chess@0.6.0/src/core/logic/boardLayouts/buildStandardBoard.ts";
 import {perft as deno_chess_perft} from "https://deno.land/x/chess@0.6.0/src/core/logic/perft.ts"
 import {Chess as ChessJS} from "https://raw.githubusercontent.com/jhlywa/chess.js/52f7579e927fb4bd2d51f3d3f07e7d2200770ddd/src/chess.ts"
+import { EtherealInterface } from "src/Engine/EtherealInterface.ts";
 
 /**
  * How fast is it compared to other engines?
  */
 
 const perftRunner = new PerftRunner('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
-Deno.bench('App Perft5', {group: 'perft5', baseline: true}, () => {
+Deno.bench('PawnStorm Perft5', {group: 'perft5', baseline: true}, () => {
     perftRunner.run(5, false)
 })
 
@@ -26,4 +27,9 @@ Deno.bench('ChessJS Perft5', {group: 'perft5'},  () => {
 const stockfish = new StockfishInterface()
 Deno.bench('Stockfish Perft5', {group: 'perft5'}, async () => {
     await stockfish.perft(5)
+})
+
+const ethereal = new EtherealInterface()
+Deno.bench('Ethereal Perft5', {group: 'perft5'}, async () => {
+    await ethereal.perft(5)
 })
